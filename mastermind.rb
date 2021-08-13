09f1c5b96cb7323cef39413ed411433ccc384b97
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-08-13 11:30:20 +0700

    Add function to Codebreaker and Codemaker class

--- mastermind.rb
+++ mastermind.rb
@@ -1,20 +1,62 @@
 # frozen_string_literal: true
 
+module Enumerable
+  def count_by(&block)
+    list = group_by(&block).map { |key, items| [key, items.count] }.sort_by(&:last)
+    Hash[list]
+  end
+end
+
 module Helper
-    # vod => arr_of_matched_idx
-    def matched(a_arr, b_arr)
-      arr_of_matched_idx = [];
-      a_arr.each_with_index do |e, i|
-        if e == b[i]
-          arr_of_matched_idx.push(i)
-        end
+  # vod => arr_of_matched_idx
+  def get_match_pos_value_indexes(a_arr, b_arr)
+    arr_of_matched_idx = []
+    a_arr.each_with_index do |e, i|
+      arr_of_matched_idx.push(i) if e == b_arr[i]
+    end
+    arr_of_matched_idx
+  end
+
+  def delete_with_index(idx_arr, arr_list)
+    idx_arr.each do |idx|
+      arr_list.each do |arr|
+        arr.delete_at(idx)
       end
     end
 
-    def match_value(my_arr, guess_arr)
-      my_arr.group_by
+    arr_list
+  end
+
+  # return num of element guess_arr extacly match value of my_arr
+  def match_value(my_arr, guess_arr)
+    count = 0
+    counted_my_arr = my_arr.count_by { |x| x }
+    counted_guess_arr = guess_arr.count_by { |x| x }
+    counted_guess_arr.each do |k, _v|
+      count += [counted_my_arr[k], counted_guess_arr].min if counted_my_arr.key? k
     end
-    
+    # intersection 2 hash to find min
+    # counted_my_arr.select { |k, _v| counted_guess_arr.key? k }
+    #               .map { |k, v| [k, [v, counted_guess_arr[k]].min] }
+    #               .to_h
+    count
+  end
+
+  def count_matched_pos_only(target_arr, compare_arr)
+    target_arr.filter.with_index { |_e, i| target_arr[i] == compare_arr[i] }.length
+  end
+
+  def count_matched_value_not_pos(target_arr, compare_arr)
+    temp_compare_arr = compare_arr.clone
+    target_arr.reject.with_index{ |_e, i| target_arr[i] == compare_arr[i] }.reduce(0) do |a, v|
+      index = temp_compare_arr.index v
+      unless index.nil?
+        temp_compare_arr.delete_at index
+        return a + 1
+      end
+      a
+    end
+  end 
 end
 
 module Mastermind
@@ -34,18 +76,18 @@ module Mastermind
       elements.clone
     end
 
-    # return {correct_both: ; correct_color_not_position: }
-    def compare_with(b_code)
-      my_element = elements
-      b_elements = b_code.elements
-      correct_both = 0
-      correct_color_not_position = 0;
-      
-      matched_idxs = Helper.matched(my_element, b_elements)
-
+    # return [correct_both, correct_value_not_position]
+    def compare_with(guess_code)
+      my_elements = elements
+      guess_elements = guess_code.elements
+      matched_position_indexes = Helper.get_match_pos_value_indexes
+      filter_my_elements = my_elements.clone
+      filter_guess_elements = guess_elements.clone
+      filter_guess_elements, filter_my_elements = Helper.delete_with_index(matched_position_indexes,
+                                                                           [filter_guess_elements, filter_my_elements])
+      match_value_count = Helper.match_value(filter_my_elements, filter_guess_elements)
+      [matched_position_indexes.length, match_value_count]
     end
-
-
   end
 
   class Player
@@ -55,18 +97,17 @@ module Mastermind
   end
 
   class ComputerPlayer < Player
+
+
     def initialize
       super('Computer')
-
     end
 
     def create_secret_code
       SecretCode.create_random_code
     end
 
-    def give_feedback
-      
-    end
+    def give_feedback; end
   end
 
   class CodeMaker
@@ -74,19 +115,41 @@ module Mastermind
 
     def initialize(player)
       @player = player
-      @code = nil
+      @code = []
     end
 
-    def create_secret_code
-      self.code = player.create_secret_code
-    end
+    # def create_secret_code
+    #   self.code = player.create_secret_code
+    # end
 
+
+    # return num of match_pos_only and match_value_not_pos as {black_pegs, white_pegs}
     def give_feedback(guess_code)
-      # return {}
-      
+      #   black_pegs, white_pegs = code.compare_with(guess_code)
+      #   { black_pegs: black_pegs, white_pegs: white_pegs }
+      count_matched_pos_only = Helper.count_matched_pos_only(guess_code, code)
+      count_matched_value_not_pos = Helper.count_matched_value_not_pos(guess_code, code)
+      {black_pegs: count_matched_pos_only, white_pegs: count_matched_value_not_pos}
     end
 
     private
-    attr_accessor :code
+
+    attr_reader :code
+
+    def code=(num1, num2, num3, num4)
+      @code = [num1, num2, num3, num4]
+    end
+  end
+
+  class CodeBreaker
+    attr_accessor :player
+
+    def initialize(player)
+      @player = player
+    end
+
+    def give_guess_code
+      @player.give_guess_code
+    end
   end
 end
