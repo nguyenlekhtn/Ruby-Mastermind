dc277b0173a6c4f74b40c985794972ed584143c6
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-08-23 21:26:47 +0700

    Add helper functions

---
+++ helper.rb
@@ -0,0 +1,32 @@
+class Hash
+  def intersection(other)
+    each_with_object({}) do |(k, v), a|
+      a[k] = [v, other[k]].min if other.key? k
+    end
+  end
+end
+
+module Enumerable
+  def count_by(&block)
+    list = group_by(&block).map { |key, items| [key, items.count] }.sort_by(&:last)
+    list.to_h
+  end
+end
+
+def count_matched_value_only(a_arr, b_arr)
+  count_matched_pos(a_arr, b_arr) - count_matched_value(a_arr, b_arr)
+end
+
+def count_matched_pos(a_arr, b_arr)
+  a_arr.select.with_index { |_value, index| a_arr[index] = b_arr[index] }.length
+end
+
+def count_matched_value(a_arr, b_arr)
+  counted_a = a_arr.count_by { |v| v }
+  counted_b = b_arr.count_by { |v| v }
+  counted_a.intersection(counted_b).values.sum
+end
+
+def counted_matched_only_value(a_arr, b_arr)
+  count_matched_value(a, b) - count_matched_pos(a_arr, b_arr)
+end
--- mastermind.rb
+++ mastermind.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require_relative 'helper'
+
 module Mastermind
   TURNS = 12
   PEGS = 4
@@ -7,30 +9,45 @@ module Mastermind
 
   # Game of mastermind, init with number of pegs and number of colors correspondingly
   class Game
-    def initialize(_pegs, _colors)
-      @history = Array(TURNS) { { guess_code: nil, feedback: nill } }
+    def initialize
+      @history = Array.new(TURNS) { { guess_code: nil, feedback: nil } }
       @valid_codes = []
+      @current_turn = 0
+      @codemaker = CodemakerAI.new self
+      @codebreaker = CodebreakerHuman.new
     end
 
-    attr_reader :pegs, :colors, :valid_codes, :codebreaker, :codemaker
+    attr_reader :pegs, :colors, :valid_codes, :codebreaker, :codemaker, :current_turn, :history
 
     def add_history(turn_i, guess_code, black_num, white_num)
       @history[turn_i][:guess_code] = guess_code
       @history[turn_i][:feedback] = { black_num: black_num, white_num: white_num }
     end
 
+    def last_guess
+      history.dig(current_turn, :guess_code)
+    end
+
     def play_turn(turn_i)
-      guess_code = @codemaker.guess
+      guess_code = @codebreaker.guess
+      puts "#{codebreaker.name} guessed #{guess_code}"
+      (@codemaker.give_feedback guess_code) => {black_num:, white_num:}
+      puts "#{codemaker.name}'s feedback: B#{black_num}W#{white_num}"
       return true if black_num == pegs
 
-      (@codebreaker.give_feedback guess_code) => {black_num:, white_num:}
       add_history(turn_i, guess_code, black_num, white_num)
     end
 
+    def create_secret_code_phase
+      @codemaker.create_secret_code
+      puts "#{codemaker.name} created code"
+    end
+
     def start
-      @codebreaker.create_secret_code
+      create_secret_code_phase
       result = false
       TURNS.times do |turn_i|
+        @current_turn = turn_i
         result = play_turn turn_i
         break if result
       end
@@ -41,15 +58,22 @@ module Mastermind
   end
 
   class Codemaker
-    def initialize
+    def initialize(game)
       @secret_code = nil
+      @game = game
     end
 
-    attr_reader :secret_code
+    attr_reader :secret_code, :game
+
+    def give_feedback(guess_code)
+      # guess_code = game.last_guess
+      (secret_code.compare_with guess_code) => {count_match_exactly:, count_wrong_pos:}
+      { black_num: count_match_exactly, white_num: count_wrong_pos }
+    end
   end
 
   class CodemakerHuman < Codemaker
-    def initialize
+    def initialize(game)
       super
       @name = 'Human'
     end
@@ -61,13 +85,29 @@ module Mastermind
   end
 
   class CodemakerAI < Codemaker
-    def initialize
+    def initialize(game)
       super
       @name = 'AI'
     end
 
+    attr_reader :name
+
     def create_secret_code
       @secret_code = Code.create_random_code
+      puts "Secret code: #{secret_code}"
+    end
+  end
+
+  class CodebreakerHuman
+    def initialize
+      @name = 'Human'
+    end
+
+    attr_reader :name
+
+    def guess
+      print 'Input your guess: '
+      Code.from_input
     end
   end
 
@@ -96,7 +136,19 @@ module Mastermind
       random = (1..PEGS).to_a.map { rand(1..COLORS).to_s }
       Code.new(*random)
     end
+
+    # {count_match_exactly:, count_wrong_pos:}
+    def compare_with(code)
+      count_match_exactly = count_matched_pos(pegs, code.pegs)
+      count_wrong_pos = count_matched_value_only(pegs, code.pegs)
+      { count_match_exactly: count_match_exactly, count_wrong_pos: count_wrong_pos }
+    end
+
+    def to_s
+      pegs.join
+    end
   end
 end
 
-p Mastermind::Code.from_input
+game = Mastermind::Game.new
+game.start
