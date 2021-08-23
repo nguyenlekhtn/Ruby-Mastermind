f1f06172abde5be023ad5d8d13629fd91937ca6b
nguyenlekhtn <nguyenlekhtn@gmail.com>
2021-08-23 09:09:30 +0700

    Add Code class

--- mastermind.rb
+++ mastermind.rb
@@ -1,244 +1,102 @@
 # frozen_string_literal: true
 
-module Enumerable
-  def count_by(&block)
-    list = group_by(&block).map { |key, items| [key, items.count] }.sort_by(&:last)
-    Hash[list]
-  end
-end
-
-module Helper
-  DEBUG = false
-
-  def log_debug(message)
-    puts "#{self}: #{message}" if DEBUG
-  end
-  module_function :log_debug
-end
-
-module ArrayCompare
-  # extend self
-
-  # def count_matched_pos_only(target_arr, compare_arr)
-  #   target_arr.filter.with_index { |_e, i| target_arr[i] == compare_arr[i] }.length
-  # end
-
-  # def count_matched_value_not_pos(target_arr, compare_arr)
-  #   not_matched_compare_arr = compare_arr.reject.with_index { |_e, i| target_arr[i] == compare_arr[i] }
-  #   not_matched_target_arr = target_arr.reject.with_index { |_e, i| target_arr[i] == compare_arr[i] }
-  #   # require 'pry-byebug'; binding.pry
-  #   not_matched_target_arr.reduce(0) do |a, v|
-  #     index = not_matched_compare_arr.index v
-  #     if index
-  #       not_matched_compare_arr.delete_at index
-  #       a + 1
-  #     else
-  #       a
-  #     end
-  #   end
-  # end
-
-  def not_match_pos(target_arr, compare_arr)
-    not_matched_compare_arr = compare_arr.reject.with_index { |_e, i| target_arr[i] == compare_arr[i] }
-    not_matched_target_arr = target_arr.reject.with_index { |_e, i| target_arr[i] == compare_arr[i] }
-    [not_matched_target_arr, not_matched_compare_arr]
-  end
-
-  def match_value_idx_arr(target_arr, compare_arr)
-    # require 'pry-byebug'; binding.pry
-    compare_arr.each_with_object([]).with_index do |(c_value, arr), idx|
-      target_arr.each do |t_value|
-        arr << idx if c_value == t_value && !arr.include?(idx)
-      end
-    end
-  end
-
-  def count_matched_pos_only(target_arr, compare_arr)
-    target_arr.length - not_match_pos(target_arr, compare_arr)[0].length
-  end
-
-  def count_matched_value_not_pos(target_arr, compare_arr)
-    match_value_idx_arr(*not_match_pos(target_arr, compare_arr)).length
-  end
-
-  # def compare_arr(a_arr, b_arr)
-  #   match_pos_indexes = []
-  #   match_value_indexes = []
-  #   a_arr.each_with_index do |a, a_idx|
-  #     b_arr.each_with_index do |b, b_idx|
-  #       if a == b && a_idx == b_idx
-  #         match_pos_indexes << b_idx
-  #       elsif a == b
-  #         match_value_indexes << b_idx unless match_pos_indexes.include? b_idx
-  #       end
-  #     end
-  #   end
-  #   { match_pos_indexes: match_pos_indexes, match_value_indexes: match_value_indexes }
-  # end
-  module_function :count_matched_pos_only, :not_match_pos, :match_value_idx_arr, :count_matched_value_not_pos
-end
-
 module Mastermind
-  CODE_SIZE = 4
-  COLOR_NUM = 6
   TURNS = 12
+  PEGS = 4
+  COLORS = 6
 
-  class CodeBreaker
-    def initialize; end
-    # def give_guess_code
-    #   @player.give_guess_code(game.guess_history)
-    # end
-  end
-
-  class CodeBreakerHuman < CodeBreaker
-    def guess_code
-      code = 1111
-      print 'Put your guess code (1122): '
-      loop do
-        code = gets.chomp
-        Helper.log_debug "#{__method__} guess code input: #{code}"
-        break if code.match(/[1-#{COLOR_NUM}]{4}/)
-
-        print 'Invalid code, type again: '
-      end
-      code.split('')
-    end
-  end
-
-  # CodeBreaker - Compuer Player
-  class CodeBreakerAI < CodeBreaker
-    # last guess info: match_pos_hash, math_color_arr
-    # match_pos_hash: pos:color
-    def guess_with_hint(last_guess_info)
-      
-    end
-
-    def guess_code(last_guess_info)
-      puts 'AI is guessing'
-      if turn.zero?
-        (1..CODE_SIZE).map { rand(1..COLOR_NUM).to_s }
-      else
-        guess_with_hint last_guess_info
-      end
-    end
-  end
-
-  class CodeMaker
-    def initialize
-      @code = []
-    end
-
-    include ArrayCompare
-
-    def feedback(guess_code)
-      #   black_pegs, white_pegs = code.compare_with(guess_code)
-      #   { black_pegs: black_pegs, white_pegs: white_pegs }
-      count_matched_pos_only = count_matched_pos_only(guess_code, @code)
-      count_matched_value_not_pos = count_matched_value_not_pos(guess_code, @code)
-      { black_pegs: count_matched_pos_only, white_pegs: count_matched_value_not_pos }
-    end
-
-    def match_secret_code?(guess_code)
-      count_matched_pos_only(@code, guess_code) == CODE_SIZE
-    end
-  end
-
-  class CodeMakerAI < CodeMaker
-    def create_secret_code(code = nil)
-      @code = if code.nil?
-                (1..CODE_SIZE).map { rand(1..COLOR_NUM).to_s }
-              else
-                code.to_s.split('')
-              end
-      puts 'CodeMaker created secret code'
-      puts @code.join
-    end
-  end
-
-  class CodeMakerHuman < CodeMaker
-    def hint guess_code
-      match_pos_hash = Hash.new(0)
-      guess_code.each_with_index do |_e, idx|
-        if guess_code[idx] == @code[idx]
-          match_pos_hash[idx] = guess_code[idx]
-        else
-
-        end
-      end
-    end
-  end
-
+  # Game of mastermind, init with number of pegs and number of colors correspondingly
   class Game
-    include Helper
+    def initialize(_pegs, _colors)
+      @history = Array(TURNS) { { guess_code: nil, feedback: nill } }
+      @valid_codes = []
+    end
 
+    attr_reader :pegs, :colors, :valid_codes, :codebreaker, :codemaker
+
+    def add_history(turn_i, guess_code, black_num, white_num)
+      @history[turn_i][:guess_code] = guess_code
+      @history[turn_i][:feedback] = { black_num: black_num, white_num: white_num }
+    end
+
+    def play_turn(turn_i)
+      guess_code = @codemaker.guess
+      return true if black_num == pegs
+
+      (@codebreaker.give_feedback guess_code) => {black_num:, white_num:}
+      add_history(turn_i, guess_code, black_num, white_num)
+    end
+
+    def start
+      @codebreaker.create_secret_code
+      result = false
+      TURNS.times do |turn_i|
+        result = play_turn turn_i
+        break if result
+      end
+      if result then puts "#{codebreaker.name} won"
+      else puts "#{codemaker.name} won"
+      end
+    end
+  end
+
+  class Codemaker
     def initialize
-      @code_breaker = CodeBreakerHuman.new
-      @code_maker = CodeMakerAI.new
-      @guess_attemps = Array.new(TURNS) { [] }
-      @feedback_log = Array.new(TURNS) { nil }
-      @turn = 1
+      @secret_code = nil
     end
 
-    def play
-      # require 'pry-byebug'; binding.pry
-      code_maker.create_secret_code 1234
-      while turn <= TURNS
-        puts "\nTurn #{turn}"
-        place_guess
-        if code_matched?
-          puts 'Codebreaker won'
-          return
-        end
-        if turn == TURNS
-          puts 'Codemaker won'
-          return
-        end
-        place_feedback
-        @turn += 1
+    attr_reader :secret_code
+  end
+
+  class CodemakerHuman < Codemaker
+    def initialize
+      super
+      @name = 'Human'
+    end
+
+    def create_secret_code
+      print 'Enter secret code: '
+      @secret_code = Code.from_input
+    end
+  end
+
+  class CodemakerAI < Codemaker
+    def initialize
+      super
+      @name = 'AI'
+    end
+
+    def create_secret_code
+      @secret_code = Code.create_random_code
+    end
+  end
+
+  class Code
+    def initialize(num1, num2, num3, num4)
+      @pegs = [num1, num2, num3, num4]
+    end
+
+    attr_accessor :pegs
+
+    def self.valid?(input)
+      input.match?(/^[1-#{COLORS}]{#{PEGS}}$/)
+    end
+
+    def self.from_input
+      input = ''
+      loop do
+        input = gets.chomp
+        break if Code.valid? input
       end
-      puts 'Codemaker won'
+      Code.new(*input.chars)
     end
 
-    private
-
-    attr_accessor :turn, :guess_attemps, :feedback_log
-    attr_reader :code_breaker, :code_maker
-
-    def print_board
-      if turn == 1
-        puts 'First attempt: '
-      else
-        puts 'Board: '
-        (1...turn).each do |i|
-          black_pegs = feedback_log[i - 1][:black_pegs]
-          white_pegs = feedback_log[i - 1][:white_pegs]
-          puts "Turn #{i}, Guess attempt: #{guess_attemps[i - 1].join('')}, black: #{black_pegs} - white: #{white_pegs}"
-        end
-      end
-    end
-
-    def place_guess
-      print_board
-      guess_code = code_breaker.guess_code
-      Helper.log_debug "#{__method__} guess_code: #{guess_code}"
-      guess_attemps[turn - 1] = guess_code
-    end
-
-    def place_feedback
-      feedback = code_maker.feedback(guess_attemps[turn - 1])
-      log_debug "#{__method__} feedback: #{feedback}"
-      feedback_log[turn - 1] = feedback
-      puts "Feedback: black: #{feedback[:black_pegs]} - white: #{feedback[:white_pegs]}"
-    end
-
-    def code_matched?
-      code_maker.match_secret_code? guess_attemps[turn - 1]
+    def self.create_random_code
+      # create a 4 pegs random number from 1 to PEGS
+      random = (1..PEGS).to_a.map { rand(1..COLORS).to_s }
+      Code.new(*random)
     end
   end
 end
 
-new_game = Mastermind::Game.new
-new_game.play
-# a = [4, 1, 4, 1]
-# b = [4, 0, 1, 4]
-# p ArrayCompare.count_matched_value_not_pos(a, b)
+p Mastermind::Code.from_input
